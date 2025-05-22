f562260fc1492d79f85bf7744f7e53bb63e3cf69
Mathilde Perrissin <mathildeperrissin@gmail.com>
2025-05-22 10:32:23 +0200

    modif p2

--- pages/dashboard_zoom_battery.py
+++ pages/dashboard_zoom_battery.py
@@ -15,7 +15,47 @@ st.title("🔍 Dashboard Zoom sur une batterie")
 # ========== 📦 Charger infos batteries ==========
 @st.cache_data
 def load_infos():
-    query = "SELECT * FROM `beem-data-warehouse.test_Mathilde.battery_actives_infos`"
+    query = """
+        WITH device_user_data AS (
+     SELECT 
+        *
+      FROM `beem-data-warehouse.airbyte_postgresql.battery_device` AS d
+      LEFT JOIN `beem-data-warehouse.airbyte_postgresql.battery_live_data` AS ld ON ld.battery_id = d.id
+      LEFT JOIN `beem-data-warehouse.airbyte_postgresql.house_user` AS hu ON d.house_id = hu.house_id 
+      LEFT JOIN `beem-data-warehouse.airbyte_postgresql.user` AS u ON hu.user_id = u.id
+      LEFT JOIN `beem-data-warehouse.airbyte_postgresql.house` AS h ON h.id = hu.house_id
+      WHERE d.deleted_at IS NULL
+        AND d.replaced_by_id IS NULL
+        AND d.warranty_status = 'activated'
+        AND d.serial_number NOT IN ('021LOLL190154M','021LOLF080008M')
+        --AND u.id NOT IN (22, 4395, 34538)
+        --AND d.hardware_version = 'ampace_v1'
+    ),
+
+    serial_counts AS (
+      SELECT 
+        serial_number,
+        COUNT(*) AS nb
+      FROM device_user_data
+      GROUP BY serial_number
+    ),
+    final AS (
+      SELECT dud.*
+      FROM device_user_data dud
+      JOIN serial_counts sc ON dud.serial_number = sc.serial_number
+      WHERE 
+        -- si le serial est unique, on garde tout
+        sc.nb = 1
+
+        -- si le serial est dupliqué, on garde seulement si email ne se termine pas par @beemenergy
+        OR (
+        sc.nb > 1
+       AND dud.email NOT LIKE '%@beemenergy.com'
+        AND dud.email NOT LIKE '%@beemenergy.fr'
+      )
+    )
+    SELECT * FROM final;
+    """
     df = client.query(query).to_dataframe()
     df.rename(columns={"id": "device_id"}, inplace=True)
     return df.dropna(subset=["device_id"])
