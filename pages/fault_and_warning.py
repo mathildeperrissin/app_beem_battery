060936a9a09f2a8275080c4b6fd8b1f29cc20942
Mathilde Perrissin <mathildeperrissin@gmail.com>
2025-05-26 16:29:17 +0200

    correctioi

--- pages/fault_and_warning.py
+++ pages/fault_and_warning.py
@@ -7,51 +7,39 @@ client = bigquery.Client()
 @st.cache_data
 def load_faulty_inverters():
     query = """
-        WITH device_user_data AS (
+        SELECT 
+            serial_number,
+            firstname,
+            lastname,
+            working_mode_code,
+            last_known_measure_date
+        FROM (
             SELECT 
-                d.serial_number,
-                u.firstname,
-                u.lastname,
-                ld.working_mode_code,
-                ld.last_known_measure_date,
-                u.email
+                d.serial_number AS serial_number,
+                u.firstname AS firstname,
+                u.lastname AS lastname,
+                u.id AS user_id,
+                ld.last_known_measure_date AS last_known_measure_date,
+                ld.working_mode_code AS working_mode_code
             FROM `beem-data-warehouse.airbyte_postgresql.battery_device` AS d
             LEFT JOIN `beem-data-warehouse.airbyte_postgresql.battery_live_data` AS ld ON ld.battery_id = d.id
-            LEFT JOIN `beem-data-warehouse.airbyte_postgresql.house_user` AS hu ON d.house_id = hu.house_id
-            LEFT JOIN `beem-data-warehouse.airbyte_postgresql.user` AS u ON hu.user_id = u.id
-            LEFT JOIN `beem-data-warehouse.airbyte_postgresql.house` AS h ON h.id = hu.house_id
+            INNER JOIN `beem-data-warehouse.airbyte_postgresql.house` AS h ON h.id = d.house_id
+            INNER JOIN `beem-data-warehouse.airbyte_postgresql.house_user` AS hu ON hu.house_id = h.id
+            INNER JOIN `beem-data-warehouse.airbyte_postgresql.user` AS u ON u.id = hu.user_id
             WHERE d.deleted_at IS NULL
               AND d.replaced_by_id IS NULL
               AND d.warranty_status = 'activated'
-              AND d.serial_number NOT IN ('021LOLL190154M', '021LOLF080008M')
-        ),
-        serial_counts AS (
-            SELECT 
-                serial_number,
-                COUNT(*) AS nb
-            FROM device_user_data
-            GROUP BY serial_number
-        ),
-        final_filtered AS (
-            SELECT dud.*
-            FROM device_user_data dud
-            JOIN serial_counts sc ON dud.serial_number = sc.serial_number
-            WHERE sc.nb = 1
-               OR (
-                   sc.nb > 1
-                   AND dud.email NOT LIKE '%@beemenergy.com'
-                   AND dud.email NOT LIKE '%@beemenergy.fr'
-               )
-        )
-        SELECT *
-        FROM final_filtered
-        WHERE working_mode_code NOT IN (
-            'ampace_v1_on_grid_discharge',
-            'ampace_v1_on_grid_charge',
-            'ampace_v1_on_grid_passby'
-        )
-          AND working_mode_code NOT LIKE 'ampace_v2%'
-          AND serial_number IS NOT NULL
+              AND u.id != 22
+              AND u.id != 4395
+              AND ld.working_mode_code NOT IN (
+                'ampace_v1_on_grid_discharge',
+                'ampace_v1_on_grid_charge',
+                'ampace_v1_on_grid_passby',
+                'ampace_v2_normal'
+              )
+              AND ld.working_mode_code NOT LIKE 'ampace_v2%'
+        ) AS virtual_table
+        ORDER BY last_known_measure_date ASC
     """
     return client.query(query).to_dataframe()
 
@@ -63,7 +51,7 @@ faulty_df = load_faulty_inverters()
 
 # Affichage du tableau
 st.dataframe(
-    faulty_df.sort_values("last_known_measure_date")[[
+    faulty_df[[  # Pas besoin de trier dans Streamlit si déjà fait dans SQL
         "serial_number", "firstname", "lastname", "working_mode_code", "last_known_measure_date"
     ]]
 )
