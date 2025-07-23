98ddf19c7b085c07fa38ac717f8e213e39a14e23
Mathilde Perrissin <mathildeperrissin@gmail.com>
2025-07-23 11:23:53 +0200

    dqs

--- 1_data_explo.py
+++ 1_data_explo.py
@@ -10,7 +10,7 @@ import plotly.graph_objects as go
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\floch\OneDrive\Documents\GCP_key\streamlit_app\beem-data-warehouse-14a923c674a0.json"
 client = bigquery.Client()
 
-st.set_page_config(page_title="Zoom Battery", layout="wide")
+st.set_page_config(page_title="BART - data explo", layout="wide")
 st.title("🔍 Data exploration one battery")
 
 # ========== 📦 Charger infos batteries ==========
